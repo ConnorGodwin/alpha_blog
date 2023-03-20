a0f8e05f54c7f40a2c1209f9290e736d442462f0
ConnorGodwin <connor.godwin@hotmail.com>
2023-03-20 17:08:31 +0000

    Add validations

--- app/models/articles.rb
+++ app/models/articles.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-
+  validates :title, presence: true, length: { minimum: 6, maximun: 100 }
+  validates :description, presence: true, length: { minimun: 10, maximum: 300 }
 end
